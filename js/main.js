6041afc410bdc87670c340d3398e57c047c68392
Debjeet Das <debjeet194@gmail.com>
2022-07-11 00:18:58 +0530

    Responsive Test -- Currently working on the responsiveness of the site

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 $(document).ready(() => {
 
 
-
+    $(".dropdownMenu_icn").hover(() => {
+        $(".dropdownMenu_icn>i").toggleClass("fa-times")
+        $(".dropdownMenu_icn").toggleClass("addBgColor")
+        $(".dropdownMenu_con").slideToggle()
+    });
 
 
 })
